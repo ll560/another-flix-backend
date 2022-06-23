b3a81f66e916bd600e8c954cafad84a6358e00fe
ll560 <llavia6@gmail.com>
2022-06-22 21:01:06 -0400

    added bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 
 const userSchema = new Schema({
     firstName: {
@@ -37,6 +40,15 @@ const userSchema = new Schema({
     }]
 }, {
     timestamps: true
+})
+
+userSchema.pre('save', async function(next){
+    //only run this function if password was modified
+    //if the password is not modified then return next
+    if(this.isModified('password')) return next()
+    //user updated password, code runs below
+    this.password = await bcrypt.hash(this.password, saltRounds) //hash method turns the password into random strings
+    return next()
 })
 
 module.exports = mongoose.model('User', userSchema)
